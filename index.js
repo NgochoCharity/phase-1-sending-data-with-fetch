a0d32173cd0de40ef181a4448e2ae27d95218cde
Charity Wanjiku Ngocho <ngochocharity@gmail.com>
2022-06-15 06:18:15 +0300

    commit tree 450959a9ef50e96c35e127b4c0e461300f9ee9e9

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Add your code here
+const dataToSubmit = {
+    name : "Steve",
+    email : "steve@steve.com"
+}
+
+const ObjData = {
+    method  : "POST",
+    headers : {
+        "Content-Type" : "application/json",
+        Accept : "application/json",
+    },
+    body : JSON.stringify(dataToSubmit),
+};
+
+
+
+function submitData(){
+  return  fetch('http://localhost:3000/users', ObjData)
+    .then(res => res.json())
+    .then(toAppend)
+    .catch(toAppendError)
+}
+
+submitData()
+
+
+function toAppend(data){
+    let body = document.querySelector("main")
+    let p = document.createElement('p')
+    p.innerHTML = `${data.id}`;
+    body.appendChild(p) 
+}
+
+function toAppendError(data){
+    let body = document.querySelector("body")
+    let p = document.createElement('p')
+    p.innerHTML = data.message;
+    body.appendChild(p) 
+}
